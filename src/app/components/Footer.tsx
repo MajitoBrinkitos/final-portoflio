9d598a556fc0dc4833f80f0f3f4b659249342f36
Majito Brinkitos <mcvef2013@gmail.com>
2025-06-14 16:50:07 -0700

    home page completed

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return(
+        <>
+        <footer className="bottom-0 left-0 z-20 w-full p-4 bg-[#494949] border-t border-gray-200 shadow-sm flex justify-center items-center md:p-6">
+            <span className="text-[#fffdf6] sm:text-center md:text-center lg:text-center">
+                © 2025 | All rights reserved.
+            </span>
+        </footer>
+        </>
+    );
+}
+
+export default Footer
