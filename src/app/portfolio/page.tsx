f42d204403fe31d21a58194e340156bfa73692a1
Majito Brinkitos <mcvef2013@gmail.com>
2025-06-14 22:18:08 -0700

    portfolio in progress

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -32,34 +32,48 @@ function Portfolio(){
     /*Grid */
         const projects = [
     {
-        title: "Modern Landing Page",
-        description: "Responsive front-end built with React, Tailwind CSS, and GSAP animations.",
-        image: "/images/portfolio/landingpage.png",
+        title: "Note and Go",
+        description: "Minimalist Note taking app built with EJS and Bootstrap5.",
+        image: "/images/portfolio/note-and-go.png",
         demo: "https://your-demo-link.com",
-        code: "https://github.com/your-repo/landing-page",
+        code: "https://github.com/MajitoBrinkitos/note-taking-app.git",
+        year: "2024",
     },
     {
-        title: "Modern Landing Page",
-        description: "Responsive front-end built with React, Tailwind CSS, and GSAP animations.",
-        image: "/images/portfolio/landingpage.png",
+        title: "Card Flipping Game",
+        description: "Simple but fun memory game built with React.",
+        image: "/images/portfolio/card-flipping.png",
+        demo: "https://majitobrinkitos.github.io/card-flipping-game/",
+        code: "https://github.com/MajitoBrinkitos/card-flipping-game.git",
+        year: "2024",
+    },
+        {
+        title: "Cielito Lindo Cafe",
+        description: "Restaurant Cielito Lindo coffee shop menu built with Bootstrap and Sass.",
+        image: "/images/portfolio/cielito-lindo.png",
         demo: "https://your-demo-link.com",
-        code: "https://github.com/your-repo/landing-page",
+        code: "https://github.com/MajitoBrinkitos/cafe-menu.git",
+        year: "2024",
     }
     ];
 
     return(
         <>
-        <div id="portfolio" className="bg-[#f9f9f9] px-4 sm:px-6 lg:px-20 py-40">
+        <div id="portfolio" className="bg-[#f2f4fb] px-4 sm:px-6 lg:px-20 py-40">
             <div className="max-w-screen-xl mx-auto text-center mb-12 py-10 ">
                 <p className="flex justify-center items-center capitalize text-[20px] text-[#ff2400]">LATEST PROJECTS</p>
                 <p ref={text1} className="text-7xl font-[Lexend Deca] my-5">A project says more than a thousand words</p>
                 <p ref={text2} className="text-4xl font-[Rochester]">What I have done, what I'm currently creating, and what my mind would like to see..</p>
             </div>
 
+            <div className="max-w-screen-xl text-start mb-12">
+                <span className="text-[20px] text-[#ff2400] font-[Lexend Deca]">Featured</span>
+            </div>
+
             {/*Grid */}
             <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                 {projects.map((project, index) => (
-                <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300">
+                <div key={index} className="bg-[#45315d] rounded-lg shadow-md overflow-hidden hover:shadow-violet-900 transition-shadow duration-300 p-3">
                     <Image
                     src={project.image}
                     alt={project.title}
@@ -67,14 +81,14 @@ function Portfolio(){
                     height={400}
                     className="w-full h-56 object-cover"
                     />
-                    <div className="p-6">
-                    <h3 className="text-xl font-semibold text-[#233142] mb-2">{project.title}</h3>
-                    <p className="text-[#455d7a] text-sm mb-4">{project.description}</p>
+                    <div>
+                    <h3 className="text-xl rounded-5 font-semibold text-[#f2f4fb] mb-2">{project.title} - {project.year}</h3>
+                    <p className="text-[#f2f4fb] text-sm mb-4">{project.description}</p>
                     <div className="flex gap-4">
-                        <a href={project.demo} target="_blank" rel="noopener noreferrer" className="text-[#f95959] hover:underline">
+                        <a href={project.demo} target="_blank" rel="noopener noreferrer" className="text-[#ff2400] hover:underline">
                         Live Demo
                         </a>
-                        <a href={project.code} target="_blank" rel="noopener noreferrer" className="text-[#233142] hover:underline">
+                        <a href={project.code} target="_blank" rel="noopener noreferrer" className="text-[#ff9280] hover:underline">
                         Source Code
                         </a>
                     </div>
